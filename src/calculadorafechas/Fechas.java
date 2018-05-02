e214d34f26a6857211bfa393f1e7e3d5ea08bc96
Felipe Cadavid <cadavid.felipe@hotmail.com>
2018-05-01 19:50:14 -0500

    Calcula

---
+++ src/calculadorafechas/Fechas.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadorafechas;
+
+/**
+ *
+ * @author Felipe Cadavid
+ */
+public class Fechas {
+    
+    public static void calcDiferenciaFechas(){
+        
+        
+        
+    }
+    
+    public static void calcDiferenciaTiempo(){
+        
+        
+        
+    }
+    
+    public static void formatearFecha(String laFecha){
+        
+        
+    }
+}
